b9dd2fc7698a53837db609d780f76ef7523b9fe3
Horeff <60672323+Horeff@users.noreply.github.com>
2022-10-17 14:04:28 +0200

    Update __init__.py

--- Maths/__init__.py
+++ Maths/__init__.py
@@ -2,12 +2,6 @@ __version__ = 0.1
 
 from packages import Fonctions_Usuelles, Wolf_alpha
 
-Wolf_alpha.ask("e^x/(1-e^x)")
-#g = Fonctions_Usuelles.fonction([1,2,3,4,5],[1,2,3,4,5], "", "test")
-#g.modeliser()
-g = Fonctions_Usuelles.fonction("exp(x)/(1-exp(x))","test")
-g.show()
-
 class maths():
   def __init__(self):
     self.func = Fonctions_Usuelles.fonction
