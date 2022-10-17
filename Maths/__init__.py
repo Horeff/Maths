d8a6486ff735d7cb6fed2fe0d81f4065ec12be1f
Horeff <60672323+Horeff@users.noreply.github.com>
2022-10-17 14:24:44 +0200

    Update __init__.py

--- Maths/__init__.py
+++ Maths/__init__.py
@@ -2,69 +2,6 @@ __version__ = 0.1
 
 from packages import Fonctions_Usuelles, Wolf_alpha
 
-import matplotlib.pyplot as plt
-import time
-import math
-import sympy as sp
-import importlib
-import os
-
-sp.init_printing()
-
-"""
-expressions (param e) :
-[str]
-conv : 
-/ = diviser
-* = multiplier
-+ = additionner
-- = soustraire
-() = parenthèses de priorité
-x = sp.Symbol("x")
-y = sp.Symbol("y")
-"""
-
-class fonction():
-  def __init__(self, e, name):
-    self.e = sp.simplify(e)
-    self.x = sp.Symbol("x")
-    self.y = sp.Symbol("y")
-    self.name = name
-    try:
-      self.der = sp.diff(self.e, self.x)
-    except:
-      self.der = None
-    try:
-      self.int = sp.integrate(self.e, self.x)
-    except : 
-      self.int = None
-    try:
-      self.inv = sp.solve(self.e - self.y, self.x, dict = True)
-      self.inv = sp.simplify(self.inv[-1][self.x])
-    except:
-      self.inv = None
-    try:
-      self.limpinf = sp.limit(self.e, self.x, sp.oo, "+")
-    except:
-      self.limpinf = None
-    try:
-      self.limminf = sp.limit(self.e, self.x, sp.oo, "-")
-    except:
-      self.limminf = None
-
-  def img(self, ant):
-    return self.e.subs(self.x, ant)
-
-  def ant(self, img):
-    return self.inv.subs(self.y, img)
-
-  def limit(self, tend):
-    return sp.limit(self.e, self.x, tend)
-
-  def show(self):
-    sp.plot(self.e,(self.x,-6,6), title=f"Graphique de ${sp.latex(self.e)}$")
- 
-    
 class maths():
   def __init__(self):
     self.func = fonction
