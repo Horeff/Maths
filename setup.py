d070766cb08edd2be2f7ad475ce6a1da83ee11e8
Horeff <60672323+Horeff@users.noreply.github.com>
2022-10-17 13:33:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup, find_packages
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='Maths',
-    url='https://github.com/jladan/package_demo',
     author='Simon LEFRANC',
     author_email='simon.lefranc94@gmail.com',
     packages=find_packages()
-    version='0.1',
     license='Archi',
     description='Module pour les mathématiques',
     # We will also need a readme eventually (there will be a warning)
