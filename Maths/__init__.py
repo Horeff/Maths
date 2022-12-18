df8768c6e051f0c6f59cf40d343eec68c8c7c94d
Horeff <60672323+Horeff@users.noreply.github.com>
2022-12-18 13:40:40 +0100

    Update __init__.py

--- Maths/__init__.py
+++ Maths/__init__.py
@@ -1,10 +1,11 @@
 __version__ = 0.1
 
-from Maths import Fonctions_Usuelles,equatio
+from Maths import Fonctions_Usuelles,equatio,modelisations
 
 class maths():
   def __init__(self):
     self.func = Fonctions_Usuelles.fonction
     self.slve = equatio.equations
+    self.mod = modelisations.mode
 
 Maths = maths()
