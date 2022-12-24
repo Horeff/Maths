6067a88db84d44a92c70473e52359145ec9c579c
Horeff <60672323+Horeff@users.noreply.github.com>
2022-12-24 15:11:47 +0100

    Update

--- Maths/Functions.py
+++ Maths/Functions.py
@@ -13,10 +13,10 @@ class func():
     def pol(self, x, a, b, c):
         return a*x**2 + b * x + c
 
-    def pol2(self, x, a, b, c, d):
+    def pol3(self, x, a, b, c, d):
         return a * x**3 + b * x**2 + c * x + d
 
-    def pol3(self, x, a, b, c, d, e):
+    def pol4(self, x, a, b, c, d, e):
         return a * x ** 4 + b * x ** 3 + c * x**2 + d * x + e
 
 functions = func()
