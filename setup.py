dbeb698dd1b34eff3db935b5538fa08216683060
Horeff <60672323+Horeff@users.noreply.github.com>
2023-09-16 19:37:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='Maths',
     author='Simon LEFRANC',
     author_email='simon.lefranc94@gmail.com',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'sympy',
+        'Algorithmique @ git+https://github.com/Horeff/Algorithmique.git'
+    ],
 )
